4267c550c2a28458e88e09bd117fb29e77893df1
Romina Azcarate <romiazcarate05@gmail.com>
2024-10-07 15:50:50 -0300

    haciendo primeras modificaciones segunda entrega

--- js/main.js
+++ js/main.js
@@ -1,46 +1,72 @@
-// Juegos disponibles y sus precios
+// Array de juegos para comprar, hay algunos que en el momento no se encuentran con stock
 const juegos = [
-    { nombre: 'Resident Evil 2', precio: 100 },
-    { nombre: 'Metal Gear Solid', precio: 80 },
-    { nombre: 'Crash Bandicoot', precio: 50 },
-    { nombre: 'Dino Crisis', precio: 50 },
-    { nombre: 'Castlevania', precio: 40 }
+    { 
+        nombre: 'Resident Evil 2',    
+        precio: 1000,
+        desc: "Juego en stock",
+        img: 'https://coverproject.sfo2.cdn.digitaloceanspaces.com/playstation_1/ps1_residentevil2_front_eu_thumb.jpg' 
+    },
+    { 
+        nombre: 'Metal Gear Solid', 
+        precio: 800,
+        desc: "Juego en stock",
+        img: 'https://coverproject.sfo2.cdn.digitaloceanspaces.com/playstation_1/ps1_metalgearsolid_5_thumb.jpg' 
+    },
+    { 
+        nombre: 'Crash Bandicoot', 
+        precio: 550,
+        desc: "Juego en stock",
+        img: 'https://coverproject.sfo2.cdn.digitaloceanspaces.com/playstation_1/playstation_1.crashbandicoot_GB.16238841271553101855_thumb.jpg' 
+    },
+    { 
+        nombre: 'Dino Crisis', 
+        precio: 700,
+        desc: "Juego en stock",
+        img: 'https://coverproject.sfo2.cdn.digitaloceanspaces.com/playstation_1/ps1_dinocrisis_2_thumb.jpg' 
+    },
+    { 
+        nombre: 'Castlevania', 
+        precio: 600,
+        desc: "Juego en stock",
+        img: 'https://coverproject.sfo2.cdn.digitaloceanspaces.com/playstation_1/ps1_castlevaniasymphonyofthenight_1_thumb.jpg'
+    },
+    {
+        nombre: 'Metal Gear Solid', 
+        precio: 800,
+        desc: "Sin stock",
+        img: 'https://coverproject.sfo2.cdn.digitaloceanspaces.com/playstation_1/ps1_metalgearsolid_5_thumb.jpg' 
+    }
 ];
 
-let totalCompra = 0;
+const contenedorJuegos = document.getElementById("container-juegos");
+const totalCompra = document.getElementById("total-compra");
+const total = 0;
 
-// Función para calcular el total de la compra
-function calcularTotal(precio, cantidad) {
-    return precio * cantidad;
-}
+juegos.forEach((elm) =>{
+    // crea un div por cada Juego agregado, con su respectiva información
+    const div = document.createElement("div");
 
-// funcion para mostrar el total de la compra
-function mostrarTotalCompra(){
-    alert(`El total de tu compra es: $${totalCompra}. Gracias por tu compra!`);
-}
+    div.classList.add("producto");
 
-// Bucle para que el usuario seleccione juegos
-while (true) {
-    let juegoElegido = parseInt(prompt(`Buenos días usuario, ¿qué juego desea comprar?\n\n- Si quiere Resident Evil 2, ingrese = 1 \n- Si quiere Metal Gear Solid, ingrese = 2 \n- Si quiere Crash Bandicoot, ingrese = 3 \n- Si quiere Dino Crisis, ingrese = 4 \n- Si quiere Castlevania, ingrese = 5 \n- Si no quiere ningún juego de la lista, o finalizar el pedido, ingrese = 0`));
-
-    if (juegoElegido === 0) {
-        break; // Salimos del bucle si elige 0
+    if(elm.desc === "Juego en stock"){
+        div.innerHTML = `
+        <h3> ${elm.nombre}<h3>
+        <img src= "${elm.img}" >
+        <p> Precio: $${elm.precio} </p>
+        <button id="btn"> Comprar </button>
+        `;
+    contenedorJuegos.appendChild(div);
     }
 
-    if (juegoElegido >= 1 && juegoElegido <= 5) {
-        let juegoSeleccionado = juegos[juegoElegido - 1]; // Obtener el juego seleccionado, restando uno ya que el array arranca en 0
-        let cantidad = parseInt(prompt(`¿Cuántas copias de ${juegoSeleccionado.nombre} desea comprar?`), 10);
-        
-        if (cantidad > 0) {
-            totalCompra = totalCompra + calcularTotal(juegoSeleccionado.precio, cantidad); // Usar la función para sumar el precio total
-            alert(`Has agregado ${cantidad} copia(s) de ${juegoSeleccionado.nombre}. Precio total hasta ahora: $${totalCompra}`);
-        } else {
-            alert("Por favor, ingresa una cantidad válida.");
+    const button = document.getElementById("btn");
+    button.addEventListener("click", () =>{
+        let cantidad = parseInt(prompt(`Cuantas cantidad de ${elm.nombre} queres comprar?`));
+        if(cantidad >0){
+            total =+ elm.precio * cantidad
+            totalCompra.textContent = `$${totalCompra}`;
+            alert(`Agregada ${cantidad} copia(s) de ${juego.nombre}. Precio total hasta ahora: $${totalCompra}`);
+
         }
-    } else {
-        alert("Selección no válida, por favor intenta nuevamente.");
-    }
-}
+    })
 
-// Mostrar el total de la compra
-mostrarTotalCompra();
+});
